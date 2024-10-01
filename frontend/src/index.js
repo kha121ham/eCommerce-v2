db13189635dcb11499c9dc2238479686480d1b9b
Khaled Hamada <khaledhamada779@gmail.com>
2024-10-01 16:55:47 +0300

    Finish product screen and start on server

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,7 @@ import './assets/styles/bootstrap.custom.css';
 import './assets/styles/index.css';
 import App from './App';
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 import { 
   createBrowserRouter,
   createRoutesFromElements,
@@ -17,6 +18,7 @@ const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App />}>
     <Route index={true} path='/' element={<HomeScreen />} />
+    <Route path='/product/:id' element={<ProductScreen />} />
     </Route>
   )
 )
