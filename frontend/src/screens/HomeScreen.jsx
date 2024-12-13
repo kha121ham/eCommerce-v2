1ecbd8fc947087bc0dc0aff8538123f6b71134dc
Khaled Hamada <khaledhamada779@gmail.com>
2024-12-13 15:58:10 +0200

    getProducts by reduxToolkit

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,21 +1,18 @@
-import React, { useEffect, useState } from 'react';
+import { useGetProductsQuery } from '../slices/productsApiSlice';
+import Product from '../components/Product'
 import { Row, Col } from 'react-bootstrap';
-import axios from 'axios';
-import Product from '../components/Product';
 
 const HomeScreen = () => {
-  const [products,setProducts] = useState([]);
-
-  useEffect(()=>{
-    const fetchProducts = async () => {
-      const { data } = await axios.get('/api/products');
-      setProducts(data);
-    }
-    fetchProducts();
-  },[])
+const { data: products, isLoading, error } = useGetProductsQuery();
   return (
     <>
-      <h1>Latest Products</h1>
+    {isLoading ? (
+      <h2>loading...</h2>
+      ) : error ? (
+        <div>{error?.data?.message || error.error}</div>
+        ) : (
+          <>
+          <h1>Latest Products</h1>
       <Row>
         {products.map(product=>(
             <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
@@ -23,7 +20,7 @@ const HomeScreen = () => {
             </Col>
         ))
         }
-      </Row>
+      </Row></>)}
     </>
   )
 }
