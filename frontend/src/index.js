5203b0067389b502bd5467a9c5fd94be3e1d6fa6
Khaled Hamada <khaledhamada779@gmail.com>
2024-12-13 15:15:41 +0200

    Finish productController and error handling and starting in redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -12,6 +12,8 @@ import {
   Route,
   RouterProvider
  } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 import reportWebVitals from './reportWebVitals';
 
 const router = createBrowserRouter(
@@ -26,7 +28,9 @@ const router = createBrowserRouter(
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+  <Provider store={store} >
     <RouterProvider router={router}/>
+  </Provider>
   </React.StrictMode>
 );
 
