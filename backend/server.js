98358690d0e9f064a9f54e39022b5b3b2e9b8874
Khaled Hamada <khaledhamada779@gmail.com>
2025-02-09 21:59:07 +0200

    Back-End Auth

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,20 @@
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
+import cookieParser from 'cookie-parser';
 import connectDB from './config/db.js';
 const port = process.env.PORT || 5000 ;
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 const app = express();
+
+//Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//Cookie parser middleware
+app.use(cookieParser());
 
 //Connect DB 
 connectDB();
