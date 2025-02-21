6f773db56f9a9486bb076f9197d48818ad4cea75
Khaled Hamada <khaledhamada779@gmail.com>
2025-02-21 23:02:13 +0200

    List Product from admin

--- frontend/src/screens/OrderScreen.jsx
+++ frontend/src/screens/OrderScreen.jsx
@@ -1,5 +1,5 @@
 import { Link, useParams } from "react-router-dom";
-import { Row, Col, ListGroup, Image, Form, Button, Card, ListGroupItem } from "react-bootstrap";
+import { Row, Col, ListGroup, Image, Button, Card, } from "react-bootstrap";
 import Message from "../components/Message";
 import Loader from "../components/Loader";
 import { toast } from "react-toastify";
--- frontend/src/screens/ProfileScreen.jsx
+++ frontend/src/screens/ProfileScreen.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { Table, Form, Button, Row, Col, FormControl, } from 'react-bootstrap';
+import { Table, Form, Button, Row, Col, } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import Message from '../components/Message';
