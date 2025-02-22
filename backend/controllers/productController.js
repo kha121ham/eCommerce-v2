6daaefc11789b54196f1a07206c8ced14b026eae
Khaled Hamada <khaledhamada779@gmail.com>
2025-02-22 17:24:13 +0200

    Edit Product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -43,4 +43,29 @@ const createProduct = asyncHandler(async (req,res)=>{
     res.status(201).json(createProduct);
 });
 
-export { getProducts,getProductById, createProduct };
+
+// @desc    Update a product
+// @route   PUT api/products
+// @access  Private/Admin
+const updateProduct = asyncHandler(async (req,res)=>{
+    const { name, price, description, image, brand, category, countInStock } = req.body;
+    const product = await Product.findById(req.params.id);
+
+    if(product) {
+        product.name = name;
+        product.price = price;
+        product.description = description;
+        product.image = image;
+        product.brand = brand;
+        product.category = category;
+        product.countInStock = countInStock;
+
+        const updateProduct = product.save();
+        res.json(updateProduct);
+    } else {
+        res.status(404);
+        throw new Error('Product not found');
+    }
+});
+
+export { getProducts,getProductById, createProduct, updateProduct };
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -19,6 +19,7 @@ import ProfileScreen from './screens/ProfileScreen';
 import AdminRoute from './components/AdminRoute';
 import OrderListScreen from './screens/admin/OrderListScreen';
 import ProductListScreen from './screens/admin/ProductListScreen';
+import ProductEditScreen from './screens/admin/ProductEditScreen';
 import { 
   createBrowserRouter,
   createRoutesFromElements,
@@ -49,6 +50,7 @@ const router = createBrowserRouter(
     <Route path='' element={<AdminRoute />}>
       <Route path='/admin/orderlist' element={<OrderListScreen />} />
       <Route path='/admin/productlist' element={<ProductListScreen />} />
+      <Route path='/admin/product/:id/edit' element={<ProductEditScreen />} />
     </Route>
     </Route>
   )
